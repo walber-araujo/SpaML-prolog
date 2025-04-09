b6ee9c857e7b727a9cafdfc50ab7bf9b2e7b10c8
walber-araujo <walber.wesley.felix.araujo.filho@ccc.ufcg.edu.br>
2025-04-09 12:02:15 -0300

    Fix intro

--- app/main.pl
+++ app/main.pl
@@ -6,4 +6,9 @@
 
 main:-
     show_intro,
+    clear_screen,
+    write("Welcome to spaML!\n"),
+    write("This project performs spam detection using Machine Learning with the Naive Bayes algorithm.\n"),
+    write("The code was developed in Haskell by Alex, João, Vinícius, and Walber.\n"),
+    wait_for_any_key,
     menu, halt.
